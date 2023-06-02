a8e52e4b2d55a774d6ed5e27d6e2bc9d29a518c1
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-06-02 10:46:04 -0300

    Criar classe database

---
+++ src/Database.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Zaptank;
+
+use \PDO;
+
+class Database {
+
+    private $conn;
+
+    public function __construct() {
+        $this->connect();
+    }
+
+    public function connect() :void {
+        
+        $pdoConfig = "{$_ENV['DB_DRIVER']}:Server={$_ENV['DB_HOST']};Database={$_ENV['DB_NAME']};";
+
+        try {
+            $pdo = new PDO($pdoConfig, $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn = $pdo;
+        } catch (PDOException $e) {
+            $mensagem = "Drivers disponiveis: " . implode(",", PDO::getAvailableDrivers());
+            $mensagem .= "\nErro: " . $e->getMessage();
+            throw new Exception($mensagem);
+        }        
+    }
+
+    public function close() :void {
+        $this->conn = null;
+    }
+
+    public function get() :pdo {
+
+        if($this->conn == null || !is_object($this->conn)) {
+            $this->connect();
+        }
+        return $this->conn;
+    }    
+}
