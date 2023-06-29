d0ca84504037b746c27a9dcdd430869a7515917d
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-06-29 15:38:07 -0300

    Criptografar senha, validar dados post, gerar e retornar jwt hash

--- src/Controllers/AccountController.php
+++ src/Controllers/AccountController.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 use App\Zaptank\Models\Account;
+use App\Zaptank\Services\Token;
 
 class AccountController {
 
@@ -15,13 +16,19 @@ class AccountController {
     public function new(Request $request, Response $response) {
 
         $email             = $_POST['email'];
-        $password          = $_POST['password'];
+        $password          = strtoupper(md5($_POST['password']));
         $phone             = $_POST['phone'];
         $ReferenceLocation = $_POST['ReferenceLocation'];
     
         $account = new Account;
-
-        if(!empty($account->selectByEmail($email))) {
+        
+        if(empty(trim($_POST['email'])) || empty(trim($_POST['password'])) || empty(trim($_POST['phone']))) {
+            $body = json_encode([
+                'success' => false,
+                'message' => 'preencha todos os campos.',
+                'status_code' => 'empty_fields'
+            ]);
+        } else if(!empty($account->selectByEmail($email))) {
             $body = json_encode([
                 'success' => false,
                 'message' => 'e-mail em uso.',
@@ -39,11 +46,29 @@ class AccountController {
             $user = $account->selectByEmail($email);
             
             if(is_array($user) && !empty($user)) {
+
+                $uid = $user['UserId'];
+                $phone = $user['Telefone'];
+
+                $token = new Token;
+
+                $jwt_hash = $token->generateAuthenticationToken($payload = [
+                    'sub' => $uid,
+                    'email' => $email,
+                    'phone' => $phone
+                ]);
+
                 $body = json_encode([
                     'success' => true,
                     'message' => 'usuário foi cadastrado com êxito.',
                     'status_code' => 'user_registered',
-                    'data' => $user
+                    'data' => [
+                        'userId' => $uid,
+                        'email' => $email,
+                        'password' => $password,
+                        'phone' => $phone,
+                        'jwt_hash' => $jwt_hash
+                    ]
                 ]);
             } else {
                 $body = json_encode([
