9411ef44c9e1a8063696b54441dedc7c5d506f9c
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-07-05 19:10:49 -0300

    Definir método de validar token JWT

--- src/Services/Token.php
+++ src/Services/Token.php
@@ -4,13 +4,17 @@ namespace App\Zaptank\Services;
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+use Firebase\JWT\SignatureInvalidException;
 
 class Token {
 
     private $private_key;
 
+    private $public_key;
+
     public function __construct() {
         $this->private_key = $_ENV['PRIVATE_KEY'];
+        $this->public_key = $_ENV['PUBLIC_KEY'];
     }
 
     public function generateAuthenticationToken(array $payload) :string {
@@ -19,4 +23,16 @@ class Token {
 
         return $jwt;
     }
+
+    public function validate($jwt) {
+
+        try {
+            $decoded = JWT::decode($jwt, new Key($this->private_key, 'HS256'));
+            $decoded_array = (array) $decoded;
+    
+            return "Decode:\n" . print_r($decoded_array, true) . "\n";        
+        } catch (SignatureInvalidException $e) {
+            echo "Token inválido: " . $e->getMessage();
+        }
+    }    
 }
