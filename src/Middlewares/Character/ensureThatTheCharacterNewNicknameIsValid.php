1a7e5dfd88b676388169d65514b7d712bd9fd074
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-08-01 13:00:37 -0300

    Criar middleware ensureThatTheCharacterNewNicknameIsValid para validação do nickname

---
+++ src/Middlewares/Character/ensureThatTheCharacterNewNicknameIsValid.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Zaptank\Middlewares\Character;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Psr7\Response;
+
+class ensureThatTheCharacterNewNicknameIsValid {
+
+    public function __invoke(Request $request, RequestHandler $handler) :Response {
+
+        $new_nickname = $_POST['newnick'];
+
+        if(empty($new_nickname)) {
+            $body = json_encode([
+                'success' => false,
+                'message' => 'Você não digitou o nome do personagem.',
+                'status_code' => 'empty_nickname'
+            ]);
+
+            $response = new Response();
+            $response->getBody()->write($body);
+            return $response; 
+        }
+    
+        $specialChars = array(".", ",", "?", "!", "'", "\\", ":", "(", ")", "/", '"', ";", "-", "+", "<", ">", "%", "~", "€", "$", "[", "]", "{", "}", "@", "&", "#", "*", "„");
+        if(strpbrk($new_nickname, implode('', $specialChars))) {
+            $body = json_encode([
+                'success' => false,
+                'message' => 'O nome do personagem contém caracteres especiais.',
+                'status_code' => 'invalid_special_characters'
+            ]);
+
+            $response = new Response();
+            $response->getBody()->write($body);
+            return $response;
+        }
+    
+        if(mb_strlen($new_nickname) < 3 || mb_strlen($new_nickname) > 16) {
+            $body = json_encode([
+                'success' => false,
+                'message' => 'O Nome do seu personagem deve ter entre 3 a 16 caracteres.',
+                'status_code' => 'invalid_special_characters'
+            ]);
+
+            $response = new Response();
+            $response->getBody()->write($body);
+            return $response;
+        }        
+          
+        return $handler->handle($request);
+    }
+}
