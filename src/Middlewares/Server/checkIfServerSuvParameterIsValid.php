c52e8f070d1f63647e38f1c8e211624133ecb55c
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-08-12 11:52:26 -0300

    Implementar o middleware checkIfServerSuvParameterIsValid

--- src/Middlewares/Server/checkIfServerSuvParameterIsValid.php
+++ src/Middlewares/Server/checkIfServerSuvParameterIsValid.php
@@ -5,12 +5,37 @@ namespace App\Zaptank\Middlewares\Server;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Psr7\Response;
+use Slim\Routing\RouteContext;
+
+use App\Zaptank\Models\Server;
+use App\Zaptank\Helpers\Cryptography;
 
 class checkIfServerSuvParameterIsValid {
 
     public function __invoke(Request $request, RequestHandler $handler) :Response {
 
-        echo 'checkIfServerSuvParameterIsValid';
+        $routeContext = RouteContext::fromRequest($request);
+        $route = $routeContext->getRoute();
+
+        $suv = $route->getArgument('suv');
+
+        $cryptography = new Cryptography;
+        $decryptServer = $cryptography->DecryptText($suv);
+
+        $server = new Server;
+
+        if($server->search($decryptServer) == false) {
+            $body = json_encode([
+                'success' => false,
+                'message' => 'Não foi possível encontrar o servidor.',
+                'status_code' => 'invalid_suv_token'             
+            ]);
+
+            $response = new Response();
+            $response->getBody()->write($body);
+            return $response;
+        }
+
         return $handler->handle($request);
     }
 }
