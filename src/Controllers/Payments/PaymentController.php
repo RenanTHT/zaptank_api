a511c2e38ec9b94b4b6c6256435100757bb060c7
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-09-11 12:02:14 -0300

    Modificar arquivo de controlador de diretório

--- src/Controllers/Payments/PaymentController.php
+++ src/Controllers/Payments/PaymentController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Zaptank\Controllers;
+namespace App\Zaptank\Controllers\Payments;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
