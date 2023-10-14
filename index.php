ef27ec7c1b3ca8344487e694e3433c703ac9a1e3
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-10-14 10:51:18 -0300

    Subir versão index.php sem configuração de cors

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-use Slim\Psr7\Response;
+use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
@@ -21,30 +21,6 @@ $app = AppFactory::create();
 // $app->addErrorMiddleware(false, true, true);
 
 $app->setBasePath('/api');
-
-$app->add(function ($request, $handler) use ($app) {
-
-    $allowedOrigins = ['http://localhost', 'https://appws.picpay.com', 'https://api.pagar.me'];
-
-    $response = $handler->handle($request);
-    $origin = $request->getHeaderLine('Origin');
-
-    if (in_array($origin, $allowedOrigins)) {
-        return $response = $response
-            ->withHeader('Access-Control-Allow-Origin', $origin)
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    }
-
-    $body = json_encode([
-        'error' => true,
-        'message' => 'O servidor não consegue responder a sua solicitação'
-    ]);
-
-    $response = new Response;
-    $response->getBody()->write($body);
-    return $response->withStatus(403);
-});
 
 require __DIR__ . '/routes/Api.php';
 
