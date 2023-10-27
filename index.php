54a40086465f26f9afeae7d64041bbe28a107850
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-10-27 11:38:53 -0300

    Fix: problema cors

--- index.php
+++ index.php
@@ -25,7 +25,6 @@ $app->setBasePath('/api');
 $app->add(function ($request, $handler) use ($app) {
     $response = $handler->handle($request);
     return $response
-        ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
--- index_cors.php
+++ index_cors.php
@@ -31,9 +31,8 @@ $app->add(function ($request, $handler) use ($app) {
 
     if (in_array($origin, $allowedOrigins)) {
         return $response = $response
-            ->withHeader('Access-Control-Allow-Origin', $origin)
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     }
 
     $body = json_encode([
