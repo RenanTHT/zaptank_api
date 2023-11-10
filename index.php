0879a330d84d4753528df7401d8f6e3e7a5053d8
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-11-10 13:16:55 -0300

    Arquivo index.php atualizado

--- index.php
+++ index.php
@@ -6,27 +6,52 @@ error_reporting(E_ALL);
 
 date_default_timezone_set('America/Sao_Paulo');
 
-use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Psr7\Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 use Slim\Factory\AppFactory;
 use Slim\Exception\HttpNotFoundException;
+use Slim\Psr7\Stream;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use App\Zaptank\Middlewares\RequestResponseLoggerMiddleware;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
+$logger = new Logger('slim');
+$logger->pushHandler(new StreamHandler('logs/app.log', Logger::INFO));
+
 $app = AppFactory::create();
+
+$app->add(new RequestResponseLoggerMiddleware($logger));
 
 // $app->addErrorMiddleware(false, true, true);
 
 $app->add(function ($request, $handler) use ($app) {
+
+    $allowedOrigins = ['https://developer.redezaptank.com.br', 'https://appws.picpay.com', 'https://api.openpix.com.br'];
+
     $response = $handler->handle($request);
-    return $response
+    $origin = $request->getHeaderLine('Origin');
+
+    if (in_array($origin, $allowedOrigins)) {
+        return $response = $response
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    }
+
+    $body = json_encode([
+        'error' => true,
+        'message' => 'O servidor não consegue responder a sua solicitação'
+    ]);
+
+    $response = new Response;
+    $response->getBody()->write($body);
+    return $response->withStatus(403);
 });
 
 require __DIR__ . '/routes/Api.php';
